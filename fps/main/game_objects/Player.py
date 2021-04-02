560531c12022920876e23388a017a64f837dc4c1
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-02 19:21:22 +0530

    Created the Player.__init__() function

--- fps/main/game_objects/Player.py
+++ fps/main/game_objects/Player.py
@@ -2,4 +2,8 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 
 class Player(FirstPersonController):
-    pass
+    __position: tuple[int, int, int] = (0, 10, 0)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.position = self.__position
