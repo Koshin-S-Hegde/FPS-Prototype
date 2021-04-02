b56ac4bc5e43de14fca0a4b16f45b2e2d9cdc30e
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-02 20:53:21 +0530

    Changed Bullet class's default position, model, color, collider_type

--- fps/main/game_objects/gun/Bullet.py
+++ fps/main/game_objects/gun/Bullet.py
@@ -3,9 +3,9 @@ from fps.main.game_objects.GameObject import GameObject
 
 
 class Bullet(GameObject):
-    _model: str = "cube"
+    _model: str = "sphere"
     _scale: tuple[int, int, int] = (0.1, 0.1, 0.1)
-    _position: tuple[int, int, int] = (0, 0, 0)
-    _color: ursina.color.color = ursina.color.gold
-    _collider_type: str = "box"
+    _position: tuple[int, int, int] = (0, 1, 0)
+    _color: ursina.color.color = ursina.color.black
+    _collider_type: str = "sphere"
     _texture_name: str = "white_cube"
