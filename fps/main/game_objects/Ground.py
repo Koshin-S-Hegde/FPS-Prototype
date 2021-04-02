7c906754b2abdc1d202ffffe7488a5895acc7829
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-02 19:43:48 +0530

    Made the Ground class cleaner by making it inherit ursina.Entity class

--- fps/main/game_objects/Ground.py
+++ fps/main/game_objects/Ground.py
@@ -1,7 +1,7 @@
 import ursina
 
 
-class Ground:
+class Ground(ursina.Entity):
     __model: str = "cube"
     __scale: tuple[int, int, int] = (100, 0.1, 100)
     __position: tuple[int, int, int] = (0, 0, 0)
@@ -9,6 +9,13 @@ class Ground:
     __collider: str = "box"
     __texture: str = "white_cube"
 
-    def __init__(self):
-        ursina.Entity(model=self.__model, scale_x=self.__scale[0], scale_y=self.__scale[1], scale_z=self.__scale[2],
-                      position=self.__position, color=self.__color, collider=self.__collider, texture=self.__texture)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.model = self.__model
+        self.scale_x = self.__scale[0]
+        self.scale_y = self.__scale[1]
+        self.scale_z = self.__scale[2]
+        self.position = self.__position
+        self.color = self.__color
+        self.collider = self.__collider
+        self.texture = self.__texture
