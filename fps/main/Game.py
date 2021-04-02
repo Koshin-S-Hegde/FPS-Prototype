e8a9951f9f82d2bd8a6134360da22a84caa94224
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-02 19:06:47 +0530

    Implemented the main player class

--- fps/main/Game.py
+++ fps/main/Game.py
@@ -1,5 +1,7 @@
 import ursina
 
+from fps.main.game_objects.Player import Player
+
 
 def update():
     pass
@@ -7,9 +9,11 @@ def update():
 
 class Game:
     __app: ursina.Ursina
+    __player: Player
 
     def __init__(self):
         self.__window_init()
+        self.__game_objects_init()
         self.__run()
 
     def __window_init(self):
@@ -20,6 +24,9 @@ class Game:
 
     def __run(self):
         self.__app.run()
+
+    def __game_objects_init(self):
+        self.__player = Player()
 
 
 if __name__ == "__main__":
