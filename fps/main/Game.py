bf25cf5d5031eb6e11f58f11d8f930df8853fa3f
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-03 10:37:42 +0530

    Implemented KeyboardInput class

--- fps/main/Game.py
+++ fps/main/Game.py
@@ -3,6 +3,7 @@ import ursina
 from fps.main.game_objects.Ground import Ground
 from fps.main.game_objects.Player import Player
 from fps.main.game_objects.gun.Gun import Gun
+from fps.main.input_handler.KeybordInput import KeyboardInput
 
 
 class Game:
@@ -10,10 +11,12 @@ class Game:
     __player: Player
     __gun: Gun
     __ground: Ground
+    __keyboard_input: KeyboardInput
 
     def __init__(self):
         self.__window_init()
         self.__game_objects_init()
+        self.__input_handler_init()
 
     def __window_init(self):
         self.__app = ursina.Ursina()
@@ -33,9 +36,15 @@ class Game:
         self.__gun.position = (1, 2, 1)
 
     def update_object(self):
-        self.__player.update_object()
+        self.__player.update_object(self.__keyboard_input.is_right_mouse_pressed())
         self.__gun.update_object()
         self.__ground.update_object()
+
+    def __input_handler_init(self):
+        self.__keyboard_input = KeyboardInput()
+
+    def update_input_handlers(self, key: str):
+        self.__keyboard_input.update_object(key)
 
 
 if __name__ == "__main__":
@@ -44,6 +53,9 @@ if __name__ == "__main__":
 
     def update():
         game.update_object()
+
+    def input(key):
+        game.update_input_handlers(key)
 
 
     game.run()
--- fps/main/game_objects/Player.py
+++ fps/main/game_objects/Player.py
@@ -1,12 +1,14 @@
 from ursina.prefabs.first_person_controller import FirstPersonController
+from fps.main.game_objects.gun.Bullet import Bullet
 
 
 class Player(FirstPersonController):
     __position: tuple[int, int, int] = (0, 10, 0)
+    __bullets: list[Bullet]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.position = self.__position
 
-    def update_object(self):
-        pass
+    def update_object(self, is_right_mouse_pressed: bool):
+        print(is_right_mouse_pressed)
