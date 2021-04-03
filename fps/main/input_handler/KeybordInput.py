e19ba27593a76bd176da82eab513b0b207cf8b79
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-03 10:54:42 +0530

    Renamed __is_right_mouse_pressed to __right_mouse_is_pressed

--- fps/main/input_handler/KeybordInput.py
+++ fps/main/input_handler/KeybordInput.py
@@ -1,11 +1,11 @@
 class KeyboardInput:
-    __is_right_mouse_pressed: bool = False
+    __right_mouse_is_pressed: bool = False
 
     def update_object(self, key: str):
         if key == "right mouse up":
-            self.__is_right_mouse_pressed = False
+            self.__right_mouse_is_pressed = False
         elif key == "right mouse down":
-            self.__is_right_mouse_pressed = True
+            self.__right_mouse_is_pressed = True
 
     def is_right_mouse_pressed(self):
-        return self.__is_right_mouse_pressed
+        return self.__right_mouse_is_pressed
