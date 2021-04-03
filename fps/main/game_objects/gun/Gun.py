7f28b64fdf34e532f753c50541625ab701346ab7
Koshin-S-Hegde <koshinhegde@gmail.com>
2021-04-03 14:03:03 +0530

    Removed Gun class's collider to prevent bugs in the future

--- fps/main/game_objects/gun/Gun.py
+++ fps/main/game_objects/gun/Gun.py
@@ -7,5 +7,5 @@ class Gun(GameObject):
     _scale: tuple[int, int, int] = (0.1, 0.1, 1)
     _position: tuple[int, int, int] = (1, 1.5, 1)
     _color: ursina.color.color = ursina.color.red
-    _collider_type: str = "box"
+    _collider_type: str = ""
     _texture_name: str = "white_cube"
